98c56139ebb9684231cc98f00014d64a7d14f316
thesandybridge <mattmilla2@gmail.com>
2019-01-11 16:57:52 -0500

    edit to inputs

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,13 @@ $('#phone1').keyup(function()
 {
     this.value = this.value.replace(/^(\d{3})(\d{3})(\d{4})+$/, "($1) $2-$3");
 });
+
+$('#phone2').keyup(function()
+{
+    this.value = this.value.replace(/^(\d{3})(\d{3})(\d{4})+$/, "$1.$2.$3");
+});
+
+$('#phone3').keyup(function()
+{
+    this.value = this.value.replace(/^[1](\d{3})(\d{3})(\d{4})+$/, "+$1 ($2) $3-$4");
+});
