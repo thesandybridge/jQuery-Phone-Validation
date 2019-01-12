fc8467770a4472e2bdc9352469597e28c7fb19c0
thesandybridge <mattmilla2@gmail.com>
2019-01-11 23:59:44 -0500

    revision

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 //Regular Expressions for phone
 $(document).ready(function() {
-    $('.phoneInput').keypress(function(key) {
+    $('input').keypress(function(key) {
         if(key.charCode < 48 || key.charCode > 57) return false;
     });
 
