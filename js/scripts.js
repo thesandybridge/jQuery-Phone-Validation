09e158f8f9fad4e22f88503a5979cadc9b471dae
thesandybridge <mattmilla2@gmail.com>
2019-01-11 23:52:13 -0500

    edits to functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,11 @@
 //Regular Expressions for phone
+window.document.onload = function disableInputs() {
+    var input = document.getElementsByTagName('input');
+    if (input.value != Number) {
+        return input.disabled = true;
+    }
+}
+
 $('#phone1').keyup(function()
 {
     this.value = this.value.replace(/^(\d{3})(\d{3})(\d{4})+$/, "($1) $2-$3");
